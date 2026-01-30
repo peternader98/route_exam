32875fc2e842a44b9a83cfb9576dfdcc97bfc61d
peternader9866 <peter.nader9866@gmail.com>
2026-01-30 10:57:22 +0200

    add assets

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  static const String routeName = 'cart';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
